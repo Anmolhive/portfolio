a808c0900ee332b9930a55c4d57a1ccfd0389a49
Anmol Singh <anmol.s@signdesk.com>
2023-09-19 00:19:52 +0530

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,24 @@
-const mobileEffectDiv = document.getElementById('mobileEffectDiv');
-(()=>{
-  let screenHeight = window.screen.height;
-  let headingHeight = document.getElementById('heading').offsetHeight;
-  let divHeight = screenHeight - headingHeight;
-  divHeight -= 100;
-  mobileEffectDiv.setAttribute('style', `height: ${divHeight}px !important;`);
-  // console.log(`Heading Height = ${mobileEffectDiv}px`);
-})();
+const randemNumber = () => {
+  return Math.random() * (100 - 0) + 0;
+};
 
+const setFlyeLocation = (e) => {
+  // clearInterval(intervelId);
+  e.setAttribute("style", `top: ${randemNumber()}%; left: ${randemNumber()}%`);
+};
+const flyes = [];
+(()=>{
+  for(let i = 0; i < 40; i++) {
+    flyes.push(`flye${i}`);
+  }
+  flyes.forEach(v => {
+    setInterval(
+      setFlyeLocation,
+      3000,
+      document.getElementById(v)
+    );
+  })
+})()
 
 const coralRed = document.getElementById("coral-red");
 const turquoise = document.getElementById("turquoise");
@@ -21,153 +32,29 @@ const darkBlue = document.getElementById("dark-blue");
 const red = document.getElementById("red");
 const blackLeaves = document.getElementById("black-leaves");
 
-const flyesContainer = document.getElementById("flyesContainer");
-const svgContainer = document.getElementById("svgContainer");
-const connectContainer = document.getElementById("connectContainer");
-
-const heading = document.getElementById("heading");
-const subHeading = document.getElementById("subHeading");
 let scroll = 0;
-
 window.addEventListener("wheel", function (event) {
   let scrolledPixels = event.deltaY;
   let temp = scroll;
-  scroll = scrolledPixels + scroll;
-  if (scroll >= 0 && scroll < 500) {
-    leavesEffect(scroll);
-    scrollEffect(scroll);
-    if(scroll > 100) {
-      heading.childNodes[1].childNodes[1].classList.remove('swip-up');
-    } else if (scroll < 100) {
-      heading.childNodes[1].childNodes[1].classList.add('swip-up');
-    }
-  } else {
-    scroll = temp;
+  if (scrolledPixels + scroll > 0) scroll = scrolledPixels + scroll;
+  if (scroll >= 0 && scroll < 1000) {
+    let translateValue = scroll / 4;
+    coralRed.setAttribute(`transform`, `translate(${translateValue / 3},${translateValue})`);
+    turquoise.setAttribute(`transform`, `translate(-${translateValue},${translateValue})`);
+    royalBlue.setAttribute(`transform`, `translate(-${translateValue},${translateValue})`);
+    fuchsia.setAttribute(`transform`, `translate(-${translateValue / 0.5},${translateValue})`);
+    vividLimeGreen.setAttribute(
+      `transform`,
+      `translate(-${translateValue / 0.5},${translateValue / 3})`
+    );
+    forestGreen.setAttribute(`transform`, `translate(${translateValue},${translateValue / 2})`);
+    deepMegenta.setAttribute(
+      `transform`,
+      `translate(-${translateValue / 2},${translateValue})`
+    );
+    rust.setAttribute(`transform`, `translate(${translateValue},${translateValue})`);
+    darkBlue.setAttribute(`transform`, `translate(-${translateValue},${translateValue})`);
+    red.setAttribute(`transform`, `translate(${translateValue},${translateValue})`);
+    blackLeaves.setAttribute(`transform`, `translate(${translateValue},${translateValue / 2})`);
   }
-});
-
-let backgroundAlpha;
-let headingTop = 90;
-let subHeadingTop = 120;
-let connectContainerTop = 140;
-const leavesEffect = (scroll) => {
-  coralRed.setAttribute(`transform`, `translate(${scroll / 3},${scroll})`);
-  turquoise.setAttribute(`transform`, `translate(-${scroll},${scroll})`);
-  royalBlue.setAttribute(`transform`, `translate(-${scroll},${scroll})`);
-  fuchsia.setAttribute(`transform`, `translate(-${scroll / 0.5},${scroll})`);
-  vividLimeGreen.setAttribute(
-    `transform`,
-    `translate(-${scroll / 0.5},${scroll / 3})`
-  );
-  forestGreen.setAttribute(`transform`, `translate(${scroll},${scroll / 2})`);
-  deepMegenta.setAttribute(`transform`, `translate(-${scroll / 2},${scroll})`);
-  rust.setAttribute(`transform`, `translate(${scroll},${scroll})`);
-  darkBlue.setAttribute(`transform`, `translate(-${scroll},${scroll})`);
-  red.setAttribute(`transform`, `translate(${scroll},${scroll})`);
-  blackLeaves.setAttribute(`transform`, `translate(${scroll},${scroll / 2})`);
-  let blur = 0;
-  if (scroll > 200) blur = scroll / 50;
-  svgContainer.style.backdropFilter = `blur(${blur}px)`;
-  if (scroll / 450 > 0.1 && scroll / 450 < 0.9) {
-    backgroundAlpha = 1 - scroll / 450;
-  }
-  flyesContainer.setAttribute(
-    "style",
-    `background:rgba(0,0,0,${backgroundAlpha});`
-  );
-};
-const scrollEffect = (scroll) => {
-  if (100 - scroll / 5 > 20 && 100 - scroll / 5 < 90)
-    headingTop = 100 - scroll / 5;
-  heading.setAttribute(`style`, `top: ${headingTop}%;`);
-  let headingOpacity = backgroundAlpha + 0.5;
-  heading.style.opacity = headingOpacity;
-  subHeadingTop = headingTop + headingTop / 1.2;
-  subHeading.setAttribute(`style`, `top: ${subHeadingTop}%;`);
-  connectContainerTop = headingTop + headingTop / 1.2 + 20;
-  connectContainer.setAttribute("style", `top: ${connectContainerTop}%;`);
-};
-
-const randemNumber = () => {
-  return Math.random() * (100 - 0) + 0;
-};
-
-const setFlyeLocation = (e) => {
-  // clearInterval(intervelId);
-  e.setAttribute("style", `top: ${randemNumber()}%; left: ${randemNumber()}%`);
-};
-
-const intervelId1 = setInterval(
-  setFlyeLocation,
-  3000,
-  document.getElementById("flye1")
-);
-const intervelId2 = setInterval(
-  setFlyeLocation,
-  3000,
-  document.getElementById("flye2")
-);
-const intervelId3 = setInterval(
-  setFlyeLocation,
-  3000,
-  document.getElementById("flye3")
-);
-const intervelId4 = setInterval(
-  setFlyeLocation,
-  3000,
-  document.getElementById("flye4")
-);
-const intervelId5 = setInterval(
-  setFlyeLocation,
-  1000,
-  document.getElementById("flye5")
-);
-const intervelId6 = setInterval(
-  setFlyeLocation,
-  3000,
-  document.getElementById("flye6")
-);
-const intervelId7 = setInterval(
-  setFlyeLocation,
-  3000,
-  document.getElementById("flye7")
-);
-const intervelId8 = setInterval(
-  setFlyeLocation,
-  3000,
-  document.getElementById("flye8")
-);
-const intervelId9 = setInterval(
-  setFlyeLocation,
-  3000,
-  document.getElementById("flye9")
-);
-const intervelId10 = setInterval(
-  setFlyeLocation,
-  1000,
-  document.getElementById("flye10")
-);
-
-document.addEventListener("touchstart", function (event) {
-  touchStartY = event.touches[0].clientY;
-});
-
-document.addEventListener("touchmove", function (event) {
-  const touchCurrentY = event.touches[0].clientY;
-  const scrolledPixels = touchStartY - touchCurrentY;
-
-  let temp = scroll;
-  scroll += scrolledPixels;
-  if (scroll >= 0 && scroll < 500) {
-    leavesEffect(scroll);
-    if(scroll > 100) {
-      heading.childNodes[1].childNodes[1].classList.remove('swip-up');
-    } else if(scroll < 100 ){
-      heading.childNodes[1].childNodes[1].classList.add('swip-up');
-    }
-  } else {
-    scroll = temp;
-  }
-  
-  touchStartY = touchCurrentY;
 });
